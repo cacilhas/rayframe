d53ec284e277612f481d7df1d94ad054c1254f61
Arhimedeς Montegasppα <cacilhas@valfenda.local>
2022-04-17 22:33:50 -0300

    add OnResize event

--- rayframe.go
+++ rayframe.go
@@ -6,20 +6,30 @@ import (
 	raylib "github.com/gen2brain/raylib-go/raylib"
 )
 
+type intVector2D = struct {
+	X, Y int
+}
+
 type RayFrame struct {
 	Camera     *raylib.Camera
 	FPS        int
+	OnRezise   func(w, h int)
 	Tick       time.Time
-	WindowSize raylib.Vector2
+	WindowSize intVector2D
 }
 
-func (rf *RayFrame) Mainloop(initialScene interface{}) {
-	rf.resize()
+func (rf *RayFrame) Init(width, height int, title string) {
+	rf.WindowSize = intVector2D{X: width, Y: height}
+	raylib.InitWindow(int32(width), int32(height), title)
 	if rf.FPS > 0 {
 		raylib.SetTargetFPS(int32(rf.FPS))
 	} else {
 		raylib.SetTargetFPS(30)
 	}
+}
+
+func (rf *RayFrame) Mainloop(initialScene interface{}) {
+	rf.resize()
 	scene := initialiseScene(nil, initialScene, rf)
 	rf.Tick = time.Now()
 	for !raylib.WindowShouldClose() {
@@ -48,14 +58,17 @@ func (rf *RayFrame) tic(scene interface{}) interface{} {
 func (rf *RayFrame) resize() {
 	if raylib.IsWindowFullscreen() {
 		currentMonitor := raylib.GetCurrentMonitor()
-		rf.WindowSize = raylib.NewVector2(
-			float32(raylib.GetMonitorWidth(currentMonitor)),
-			float32(raylib.GetMonitorHeight(currentMonitor)),
-		)
+		rf.WindowSize = intVector2D{
+			X: raylib.GetMonitorWidth(currentMonitor),
+			Y: raylib.GetMonitorHeight(currentMonitor),
+		}
 	} else {
-		rf.WindowSize = raylib.NewVector2(
-			float32(raylib.GetScreenWidth()),
-			float32(raylib.GetScreenHeight()),
-		)
+		rf.WindowSize = intVector2D{
+			X: raylib.GetScreenWidth(),
+			Y: raylib.GetScreenHeight(),
+		}
+	}
+	if rf.OnRezise != nil {
+		rf.OnRezise(rf.WindowSize.X, rf.WindowSize.Y)
 	}
 }
